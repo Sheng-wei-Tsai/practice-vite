f61e74993b9f1839435c6eb397072fb9078eb8b6
Henry Tsai <henry88002605@gmail.com>
2025-03-06 14:09:09 +1000

    creating static server

---
+++ main.js
@@ -0,0 +1,10 @@
+import greeting from "./greeting"
+
+// if (typeof greeting === 'string') {
+//   let parseObject = JSON.parse(greeting)
+//   console.log(`${parseObject} Henry`)
+// } else {
+//   console.log(`${greeting} Henry`)
+// }
+
+
